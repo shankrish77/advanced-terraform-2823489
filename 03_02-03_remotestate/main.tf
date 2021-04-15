94456b828e99f997293d07ff498354580851c6e3
shankrish77 <74517850+shankrish77@users.noreply.github.com>
2021-04-15 09:25:40 -0700

    Update main.tf

--- 03_02-03_remotestate/main.tf
+++ 03_02-03_remotestate/main.tf
@@ -26,7 +26,7 @@ variable "subnet1_cidr" {
 }
 
 variable "vpc_tags" {
-  environment = "terraform-jenkins-shan"  
+  default = "environment" = "terraform-jenkins-shan"  
 }
 
 # //////////////////////////////
